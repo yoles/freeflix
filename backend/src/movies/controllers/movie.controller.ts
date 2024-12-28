1e29469bfe6fc806ffb7e0c2da6d5e3f14e17ecf
yoles <yo-0ne@hotmail.fr>
2024-12-28 09:43:49 +0100

    Rename GetTrendingMoviesUseCases into GetTrendingMovies

--- backend/src/movies/controllers/movie.controller.ts
+++ backend/src/movies/controllers/movie.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get} from '@nestjs/common';
-import { GetTrendingMoviesUseCase } from '@movies/usecases/get-trending-movies.usecase';
+import { GetTrendingMovies } from '@movies/usecases/get-trending-movies.usecase';
 
 @Controller('movies')
 export class MovieController {
     constructor(
-        private readonly getTrendingMoviesUseCase: GetTrendingMoviesUseCase
+        private readonly useCase: GetTrendingMovies
     ) {}
 
     @Get('/trend')
     async getTrending() {
-        return this.getTrendingMoviesUseCase.execute();
+        return this.useCase.execute();
     }
 }
